e38a7e55dc8f431e0034fcf0080e380fa38abc70
Alex-Parmar03 <136486520+Alex-Parmar03@users.noreply.github.com>
2024-01-23 12:20:21 +0000

    tab module add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 // navigation 
 let nav = document.querySelector(".nav-btn");
-nav.addEventListener("click", function(){
+nav.addEventListener("click", function () {
     document.querySelector(".nav-title").classList.toggle("block");
 });
 
-function secondary_nevigation(){
+function secondary_nevigation() {
     document.querySelector(".secondary-nev").classList.toggle("mega-nav-block");
 }
 
@@ -14,21 +14,50 @@ let accordionitems = document.querySelectorAll(".accordion-item");
 accordionitems.forEach(item => {
     let lable = item.querySelector(".accordion-lable");
     let content = item.querySelector(".accordion-content");
-    lable.addEventListener('click', function() {
-        for( i = 0; i < accordionitems.length; i++){
-            if(accordionitems[i] == item){
+    lable.addEventListener('click', function () {
+        for (i = 0; i < accordionitems.length; i++) {
+            if (accordionitems[i] == item) {
                 item.classList.toggle("active");
             }
-            else{
+            else {
                 accordionitems[i].classList.remove("active");
             }
-        }        
-});
+        }
+    });
 });
 
 // tabs
-let tabbtn = document.querySelector(".tab-btn");
+// let tabbtn = document.querySelectorAll(".tab-btn");
 
-tabbtn.addEventListener('click', function(){
-    document.querySelector('.tab-content').classList.toggle('block');
-})
+// tabbtn.forEach(tab => {
+//     let btnid = this.IDBIndex();
+//     tabbtn.addEventListener('click', function (){
+
+//         document.querySelector('.tab-content').classList.toggle('block');
+//     })
+// })
+
+
+var tabbtn = document.querySelectorAll(".tab-btn");
+
+    for (var i = 0; i < tabbtn.length; i++) {
+      tabbtn[i].addEventListener("click", function () {
+        var tabid = this.dataset.tab;
+        var tabcontent = document.getElementById(tabid);
+        var alltabcontent = document.querySelectorAll(".tab-content");
+        var alltabbtn = document.querySelectorAll(".tab-btn");
+
+        for (var j = 0; j < alltabcontent.length; j++) {
+          alltabcontent[j].style.display = "none";
+        }
+
+        for (var k = 0; k < alltabbtn.length; k++) {
+          alltabbtn[k].classList.remove("active");
+        }
+
+        tabcontent.style.display = "block";
+        this.classList.add("active");
+      });
+    }
+
+    document.querySelector(".tab-btn").click();
